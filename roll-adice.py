77a99375a7108af90855017eae59a58389e0a3a3
BHASKAR58900 <115164251+BHASKAR58900@users.noreply.github.com>
2024-06-30 08:00:37 +0530

    Add files via upload

---
+++ roll-adice.py
@@ -0,0 +1,34 @@
+import random
+ 
+def num(random):
+    no = random.randint(1,6)
+    if no==1:   
+      print("[-----]")
+      print("[     ]")
+      print("[  0  ]")
+      print("[     ]")
+      print("[-----]")
+    if no==2:
+     print("[-----]")
+     print("[     ]")
+     print("[ 000 ]")
+     print("[     ]")
+     print("[-----]")
+    if no==1:
+      print("[-----]")
+      print("[0   0]")
+      print("[  0  ]")
+      print("[0   0]")
+      print("[-----]")
+    if no==1:
+       print("[-----]")
+       print("[0   0]")
+       print("[     ]")
+       print("[0   0]")
+       print("[-----]")
+
+no=input("enter no ")
+num(random)
+
+
+
